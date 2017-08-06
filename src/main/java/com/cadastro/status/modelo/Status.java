cdd1deb9eced10760cf7c881bab6de756dc359f5
Daniel Ferraz <new_daniel2010@hotmail.com>
2017-08-06 14:27:37 -0300

    Criação da classe de Status do modelo da aplicação

---
+++ src/main/java/com/cadastro/status/modelo/Status.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.cadastro.status.modelo;
+
+/**
+ * Representa os dados do estado de um veículo
+ * 
+ * 
+ * @author Daniel Ferraz
+ * @since 6 de ago de 2017
+ *
+ */
+public class Status {
+
+}
---
+++ src/main/java/com/cadastro/status/modelo/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Pacote com as classes do modelo de negócio do sistema
+ * @author Daniel Ferraz
+ * @since 6 de ago de 2017
+ *
+ */
+package com.cadastro.status.modelo;
