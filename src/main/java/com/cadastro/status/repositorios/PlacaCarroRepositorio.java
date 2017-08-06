3b245a344aff6b6f940342f347d8fba4474969c4
Daniel Ferraz <new_daniel2010@hotmail.com>
2017-08-06 14:43:07 -0300

    Criação do repositorio de acesso aos dados das entidades

---
+++ src/main/java/com/cadastro/status/repositorios/PlacaCarroRepositorio.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.cadastro.status.repositorios;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cadastro.status.modelo.PlacaCarro;
+
+/**
+ * @author Daniel Ferraz
+ * @since 6 de ago de 2017
+ *
+ */
+public interface PlacaCarroRepositorio  extends CrudRepository<PlacaCarro, Long> {
+
+}
