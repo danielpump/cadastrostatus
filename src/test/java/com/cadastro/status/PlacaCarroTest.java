b45be803d05e97057551aef2b775d3ca1176c773
Daniel Ferraz <new_daniel2010@hotmail.com>
2017-08-08 00:39:45 -0300

    Inicializando implementação dos testes de integração

--- src/test/java/com/cadastro/status/PlacaCarroTest.java
+++ src/test/java/com/cadastro/status/PlacaCarroTest.java
@@ -1,5 +1,7 @@
 package com.cadastro.status;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,14 +12,16 @@ import com.cadastro.status.modelo.PlacaCarro;
 
 @RunWith(SpringRunner.class)
 public class PlacaCarroTest extends ApplicationTest {
-	
+
 	@Autowired
 	private PlacaCarroControle controle;
-	
+
 	@Test
 	public void teste() {
 		PlacaCarro buscarPorNumero = controle.buscarPorNumero("FFF5888");
-		System.out.println(buscarPorNumero);
+
+		assertThat(buscarPorNumero.getNumero()).isEqualTo("FFF5888");
+		assertThat(buscarPorNumero.getStatus()).isEqualTo("OK");
 	}
 
 }
