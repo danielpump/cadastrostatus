a69b769e50792983535c161f4304a6f70ef5252b
Daniel Ferraz <new_daniel2010@hotmail.com>
2017-08-09 00:28:06 -0300

    Criação dos testes automatizados dos cenários de sucesso

--- src/test/java/com/cadastro/status/PlacaCarroTest.java
+++ src/test/java/com/cadastro/status/PlacaCarroTest.java
@@ -2,21 +2,23 @@ package com.cadastro.status;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.io.UnsupportedEncodingException;
-import java.util.Map;
+import static org.hamcrest.Matchers.containsString;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.cadastro.status.controles.PlacaCarroControle;
-import com.cadastro.status.modelo.PlacaCarro;
 
 @RunWith(SpringRunner.class)
+@Profile("teste")
 public class PlacaCarroTest extends ApplicationTest {
 
 	@Autowired
@@ -26,7 +28,7 @@ public class PlacaCarroTest extends ApplicationTest {
 	private MockMvc mockMvc;
 
 	@Test
-	public void testeDeConsultaPorPlacaDeVeículoComStatusOK() throws Exception {
+	public void testeDeConsultaPorPlacaDeVeiculoComStatusOK() throws Exception {
 		String jsonResposta = this.mockMvc.perform(get("/placa/consultar?numero=FFF5888")).andExpect(status().isOk())
 				.andReturn().getResponse().getContentAsString();
 
@@ -35,7 +37,7 @@ public class PlacaCarroTest extends ApplicationTest {
 	}
 
 	@Test
-	public void testeDeConsultaPorPlacaDeVeículoComStatusBloqueado() throws Exception {
+	public void testeDeConsultaPorPlacaDeVeiculoComStatusBloqueado() throws Exception {
 		String jsonResposta = this.mockMvc.perform(get("/placa/consultar?numero=FFF5889")).andExpect(status().isOk())
 				.andReturn().getResponse().getContentAsString();
 
@@ -43,7 +45,7 @@ public class PlacaCarroTest extends ApplicationTest {
 	}
 
 	@Test
-	public void testeDeConsultaPorPlacaDeVeículoComStatusApreendido() throws Exception {
+	public void testeDeConsultaPorPlacaDeVeiculoComStatusApreendido() throws Exception {
 		String jsonResposta = this.mockMvc.perform(get("/placa/consultar?numero=FFF5890")).andExpect(status().isOk())
 				.andReturn().getResponse().getContentAsString();
 
@@ -51,11 +53,11 @@ public class PlacaCarroTest extends ApplicationTest {
 	}
 
 	@Test
-	public void testeDeConsultaPorPlacaDeVeículoInexistente() throws Exception {
+	public void testeDeConsultaPorPlacaDeVeiculoInexistente() throws Exception {
 		String jsonResposta = this.mockMvc.perform(get("/placa/consultar?numero=FFF5885"))
-				.andExpect(status().isBadRequest()).andReturn().getResponse().getContentAsString();
-
-		assertThat(jsonResposta).isEqualTo("{\"numero\":\"FFF5888\",\"status\":\"OK\"}");
+				.andExpect(status().isBadRequest())
+				// .andExpect(status().reason(containsString("")))
+				.andReturn().getResponse().getContentAsString();
 	}
 
 	@Test
@@ -82,4 +84,42 @@ public class PlacaCarroTest extends ApplicationTest {
 		assertThat(jsonResposta).isEqualTo("{\"quantidade\":1,\"status\":\"APREENDIDO\"}");
 	}
 
+	@Test
+	public void testeDeCadastroComStatusOK() throws Exception {
+		String jsonResposta = this.mockMvc
+				.perform(post("/placa/cadastrar").contentType(MediaType.APPLICATION_JSON_UTF8)
+						.content("{\"numero\":\"FFF5885\",\"status\":\"OK\"}"))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+		assertThat(jsonResposta).isEqualTo("{\"numero\":\"FFF5885\",\"status\":\"OK\"}");
+	}
+
+	@Test
+	public void testeDeAtualizacaoComStatusBloqueado() throws Exception {
+		this.mockMvc
+				.perform(post("/placa/cadastrar").contentType(MediaType.APPLICATION_JSON_UTF8)
+						.content("{\"numero\":\"FFF5885\",\"status\":\"OK\"}"))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+		String jsonResposta = this.mockMvc
+				.perform(post("/placa/atualizar?numero=FFF5885").contentType(MediaType.APPLICATION_JSON_UTF8)
+						.content("{\"status\":\"BLOQUEADO\"}"))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+		assertThat(jsonResposta).isEqualTo("{\"numero\":\"FFF5885\",\"status\":\"BLOQUEADO\"}");
+	}
+	
+	@Test
+	public void testeDeExclusaoDeVeiculo() throws Exception {
+		this.mockMvc
+				.perform(post("/placa/cadastrar").contentType(MediaType.APPLICATION_JSON_UTF8)
+						.content("{\"numero\":\"FFF5885\",\"status\":\"OK\"}"))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+		String jsonResposta = this.mockMvc
+				.perform(delete("/placa/excluir?numero=FFF5885"))
+				.andExpect(status().isOk()).andReturn().getResponse().getContentAsString();
+
+		assertThat(jsonResposta).isEqualTo("{\"numero\":\"FFF5885\",\"status\":\"OK\"}");
+	}
 }
